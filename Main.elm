bf0d5f8ee19da9edddb5b3317d7dbdfa4181df3a
Alex Hunley <ahunley@nutshell.com>
2017-05-30 23:52:31 -0700

    :rocket: engage!

---
+++ Main.elm
@@ -0,0 +1,62 @@
+module Main exposing (..)
+
+import Html exposing (Html, program, h1, text)
+
+
+-- MAIN
+
+
+main : Program Never Model Msg
+main =
+    program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+
+
+-- MODEL
+
+
+type alias Model =
+    String
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( "hello, world", Cmd.none )
+
+
+
+-- UPDATE
+
+
+type Msg
+    = NoOp
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        NoOp ->
+            ( model, Cmd.none )
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    h1 [] [ text model ]
